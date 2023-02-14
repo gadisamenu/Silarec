b9af27712bffb01c5c45d20b20054d28cefdd296
Amha Kindu <amhaznif@gmail.com>
2023-02-14 09:09:12 -0800

    added widget to image component

--- lib/Presentation/Screens/Videos/samplePlayer.dart
+++ lib/Presentation/Screens/Videos/samplePlayer.dart
@@ -1,7 +1,14 @@
 // ignore: file_names
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 import 'package:chewie/chewie.dart';
+import 'dart:ui' as ui;
+
+import 'package:screenshot/screenshot.dart';
 
 class SamplePlayer extends StatefulWidget {
   final String url;
@@ -14,63 +21,103 @@ class SamplePlayer extends StatefulWidget {
 
 class _SamplePlayerState extends State<SamplePlayer> {
   late VideoPlayerController videoPlayerController;
-  late ChewieController chewieController;
+  final ScreenshotController screenshotController = ScreenshotController();
+  final GlobalKey widgetKey = GlobalKey();
+
+  Uint8List? _imageFile;
 
   @override
   void initState() {
     videoPlayerController = VideoPlayerController.asset(widget.url);
-    videoPlayerController.initialize().then((value) => null);
+    videoPlayerController.initialize().then((value) => value);
+
+    // Timer.periodic(const Duration(seconds: 2), (_) {
+    //   _captureImage();
+    //   print('widgetshot taken...');
+    // });
+
     super.initState();
   }
 
   @override
   void dispose() {
     videoPlayerController.dispose();
-    chewieController.dispose();
     super.dispose();
+  }
+
+  Future<void> _captureImage() async {
+    final RenderRepaintBoundary boundary =
+        widgetKey.currentContext!.findRenderObject()! as RenderRepaintBoundary;
+
+    final ui.Image image = await boundary.toImage();
+    final ByteData? byteData =
+        await image.toByteData(format: ui.ImageByteFormat.png);
+    final Uint8List pngBytes = byteData!.buffer.asUint8List();
+    print(pngBytes.length);
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Testing')),
-        body: Container(
-          height: 400,
-          child: Chewie(
-            controller: ChewieController(
-              videoPlayerController: videoPlayerController,
-              autoInitialize: true,
-              autoPlay: true,
-              looping: true,
-              aspectRatio: videoPlayerController.value.aspectRatio,
-              // placeholder: ,
-              subtitle: Subtitles([
-                Subtitle(
-                  index: 0,
-                  start: Duration.zero,
-                  end: const Duration(seconds: 10),
-                  text: 'Hello from subtitles',
-                ),
-                Subtitle(
-                  index: 1,
-                  start: const Duration(seconds: 10),
-                  end: const Duration(seconds: 20),
-                  text: 'Whats up? :)',
-                ),
-              ]),
-              subtitleBuilder: (context, subtitle) => Container(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  subtitle,
-                  style: const TextStyle(color: Colors.white),
+    return Column(
+      children: [
+        Container(
+          height: 300,
+          child: RepaintBoundary(
+            key: widgetKey,
+            // controller: screenshotController,
+            child: Chewie(
+              controller: ChewieController(
+                videoPlayerController: videoPlayerController,
+                autoInitialize: true,
+                autoPlay: true,
+                looping: true,
+                aspectRatio: videoPlayerController.value.aspectRatio,
+                placeholder: CircularProgressIndicator(
+                    color: Theme.of(context).colorScheme.primary),
+                deviceOrientationsOnEnterFullScreen: [
+                  DeviceOrientation.landscapeRight,
+                  DeviceOrientation.landscapeRight,
+                  DeviceOrientation.portraitUp
+                ],
+                deviceOrientationsAfterFullScreen: [
+                  DeviceOrientation.portraitUp
+                ],
+                subtitle: Subtitles([
+                  Subtitle(
+                    index: 0,
+                    start: Duration.zero,
+                    end: const Duration(seconds: 10),
+                    text: 'Hello from subtitles',
+                  ),
+                  Subtitle(
+                    index: 1,
+                    start: const Duration(seconds: 10),
+                    end: const Duration(seconds: 20),
+                    text: 'Whats up? :)',
+                  ),
+                ]),
+                subtitleBuilder: (context, subtitle) => Container(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    subtitle,
+                    style: const TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
             ),
           ),
         ),
-      ),
+        ElevatedButton(
+            child: const Text(
+              'Capture',
+            ),
+            onPressed: () {
+              _captureImage();
+            }),
+        _imageFile != null
+            ? SizedBox(height: 200, child: Image.memory(_imageFile!))
+            : const Text('null')
+      ],
     );
   }
 }
--- lib/Presentation/Screens/Videos/videos.dart
+++ lib/Presentation/Screens/Videos/videos.dart
@@ -1,13 +1,15 @@
-
-
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:silarec/Presentation/Screens/Videos/samplePlayer.dart';
 
 class Videos extends StatelessWidget {
   const Videos({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    
+    return const SamplePlayer(url: 'assets/videos/file_example.webm');
   }
+
+  String get_title() => "Video";
 }
+
