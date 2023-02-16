a12127c6ecba40ed3cd35ba1a106baf85a449acd
Amha Kindu <amhaznif@gmail.com>
2023-02-16 04:27:46 -0800

    silarec model and live recording integrated

--- lib/Presentation/Screens/Live Recording/live_recording.dart
+++ lib/Presentation/Screens/Live Recording/live_recording.dart
@@ -1,8 +1,10 @@
-import 'dart:io';
-import 'dart:typed_data';
+
+import 'package:image/image.dart' as img;
 
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
+import 'package:silarec/Services/SignVidClassifier.dart';
 
 class LiveRecording extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -24,6 +26,10 @@ class _CameraPageState extends State<CameraPage> {
   bool _isRecording = false;
 
   late CameraController _cameraController;
+
+  final SignVidClassifier _classifier = SignVidClassifier();
+  
+  Category? _category;
 
   @override
   void dispose() {
@@ -53,9 +59,17 @@ class _CameraPageState extends State<CameraPage> {
       await _cameraController.stopImageStream();
       setState(() => _isRecording = false);
     } else {
-      await _cameraController.startImageStream((image) => null);
+      await _cameraController.startImageStream((image) => getPrediction(image));
       setState(() => _isRecording = true);
     }
+  }
+
+  getPrediction(CameraImage image) {
+    var pred = _classifier.predict(convertYUV420ToImage(image));
+
+    setState(() {
+      _category = pred;
+    });
   }
 
   @override
@@ -101,4 +115,50 @@ class _CameraPageState extends State<CameraPage> {
       );
     }
   }
+}
+
+
+
+img.Image convertYUV420ToImage(CameraImage cameraImage) {
+    final width = cameraImage.width;
+    final height = cameraImage.height;
+
+    final yRowStride = cameraImage.planes[0].bytesPerRow;
+    final uvRowStride = cameraImage.planes[1].bytesPerRow;
+    final uvPixelStride = cameraImage.planes[1].bytesPerPixel!;
+
+    final image = img.Image(width, height);
+
+    for (var w = 0; w < width; w++) {
+      for (var h = 0; h < height; h++) {
+        final uvIndex =
+            uvPixelStride * (w / 2).floor() + uvRowStride * (h / 2).floor();
+        final index = h * width + w;
+        final yIndex = h * yRowStride + w;
+
+        final y = cameraImage.planes[0].bytes[yIndex];
+        final u = cameraImage.planes[1].bytes[uvIndex];
+        final v = cameraImage.planes[2].bytes[uvIndex];
+
+        image.data[index] = yuv2rgb(y, u, v);
+      }
+    }
+    return image;
+  }
+
+int yuv2rgb(int y, int u, int v) {
+  // Convert yuv pixel to rgb
+  var r = (y + v * 1436 / 1024 - 179).round();
+  var g = (y - u * 46549 / 131072 + 44 - v * 93604 / 131072 + 91).round();
+  var b = (y + u * 1814 / 1024 - 227).round();
+
+  // Clipping RGB values to be inside boundaries [ 0 , 255 ]
+  r = r.clamp(0, 255);
+  g = g.clamp(0, 255);
+  b = b.clamp(0, 255);
+
+  return 0xff000000 |
+      ((b << 16) & 0xff0000) |
+      ((g << 8) & 0xff00) |
+      (r & 0xff);
 }
--- lib/Services/constants/models.dart
+++ lib/Services/constants/models.dart
@@ -1,4 +1,3 @@
 mixin ModelFile {
-  static const String singleFrame = 'assets/models/silarec_single_frame.tflite';
-  static const String multipleFrame = 'assets/models/silarec_multiple_frame.tflite';
+  static const String singleFrame = 'assets/models/SingleFrame_SILAREC_model_v0_lr_1e-04_C2C2DD_Nadam_optimizer_TRA_71.35_TEA_25.6.tflite';
 }
